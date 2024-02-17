845b0f2f55e427bdefc0b7e2b7706b83c589e1b6
Shivam Sethi <ss3618@bath.ac.uk>
2024-02-17 17:36:31 +0000

    DDPG Actor + Critic Network changes

--- src/Models/ActorNetwork.py
+++ src/Models/ActorNetwork.py
@@ -1,9 +1,13 @@
+import torch
 from torch import nn
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, input_dims, action_dims, hidden_layer_dims=512):
+    def __init__(self, optimiser, loss, input_dims, action_dims, hidden_layer_dims=512, optimiser_args=None):
         super().__init__()
+
+        self.loss = loss
+
         self.input_dims = input_dims
         self.action_dims = action_dims
         self.hidden_layer_dims = hidden_layer_dims
@@ -14,9 +18,19 @@ class ActorNetwork(nn.Module):
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
 
-    def forward(self, state):
+
+        optimiser_args = optimiser_args if optimiser_args else {}
+        self.optimiser = optimiser(self.parameters(), **optimiser_args)
+
+    def forward(self, state: torch.Tensor):
         x = self.relu(self.input_layer(state))
         x = self.relu(self.hidden_layer_1(x))
         action = self.tanh(self.output_layer(x))
 
         return action
+
+    def update(self, actor_loss):
+        self.optimiser.zero_grad()
+        loss = torch.mean(actor_loss, dim=0)
+        loss.backward()
+        self.optimiser.step()
--- src/Models/CriticNetwork.py
+++ src/Models/CriticNetwork.py
@@ -4,8 +4,11 @@ from torch import nn
 
 class CriticNetwork(nn.Module):
     # input_dims = state_dims + action_dims
-    def __init__(self, input_dims, hidden_layer_dims=512):
+    def __init__(self, optimiser, loss, input_dims, hidden_layer_dims=512, optimiser_args=None):
         super().__init__()
+
+        self.loss = loss
+
         self.input_dims = input_dims
         self.hidden_layer_dims = hidden_layer_dims
         self.input_layer = nn.Linear(self.input_dims, self.hidden_layer_dims)
@@ -14,9 +17,18 @@ class CriticNetwork(nn.Module):
 
         self.relu = nn.ReLU()
 
+        optimiser_args = optimiser_args if optimiser_args else {}
+        self.optimiser = optimiser(self.parameters(), **optimiser_args)
+
     def forward(self, state: torch.Tensor, action: torch.Tensor):
-        input_tensor = torch.cat([state, action])
+        input_tensor = torch.cat([state, action], dim=1)
         x = self.relu(self.input_layer(input_tensor))
         x = self.relu(self.hidden_layer_1(x))
         q_value = self.output_layer(x)
         return q_value
+
+    def update(self, predicted, target):
+        self.optimiser.zero_grad()
+        loss = self.loss(predicted, target)
+        loss.backward()
+        self.optimiser.step()
