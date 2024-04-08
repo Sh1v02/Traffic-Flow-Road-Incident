51dbfbf1ac1954cc922b94e816bf90ef8e308c94
Shivam Sethi <ss3618@bath.ac.uk>
2024-04-08 01:57:34 +0100

    huber loss delta = 10

--- src/AgentRunners/MAPPOAgentRunner.py
+++ src/AgentRunners/MAPPOAgentRunner.py
@@ -13,7 +13,7 @@ class MAPPOAgentRunner(AgentRunner):
     def __init__(self, env, test_env, local_state_dims, global_state_dims, action_dims, optimiser=torch.optim.Adam,
                  loss=torch.nn.MSELoss()):
         if settings.MAPPO_CRITIC_LOSS_FUNCTION.lower() == "huber":
-            loss = torch.nn.HuberLoss()
+            loss = torch.nn.HuberLoss(delta=10.0)
         self.agent = MAPPOAgent(optimiser, loss, local_state_dims, global_state_dims, action_dims)
         self.global_state_dims = global_state_dims
         super().__init__(env, test_env, self.agent)
